6714254e2052d7ffcf25bd4f311923ab04fda1df
Brian Luckau <Brian Luckau@DESKTOP-8SPURV5>
2017-06-06 17:18:15 -0600

    Implemented the second challenge

--- src/test/java/com/stg/bluckau/qa/AppTest.java
+++ src/test/java/com/stg/bluckau/qa/AppTest.java
@@ -13,31 +13,81 @@ import org.junit.Test;
 
 public class AppTest 
 {
-	private static WebDriver driver;
-	    
 
 	@BeforeClass
 	public static void beforeClass()
 	{
 		ChromeDriverManager.getInstance().setup();
 		System.out.println("before");
-		driver = new ChromeDriver();
 	}
 	
 	@AfterClass
 	public static void Afterclass()
 	{
 		System.out.println("after");
-		driver.quit();
+		TestObjects.driver.quit();
 	}
 
-	//Not big enough to require a helper or page object just yet
-    @Test
-    public void testTitle()
-    {
-    	driver.get("http://www.skiutah.com");
-    	String title = driver.getTitle();
-        System.out.println("Title is " + title);
-        assertEquals(title, "Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah");
-    }
+	@Test
+	public void testTitle()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String title = TestObjects.getPageTitle();
+		System.out.println("Title is " + title);
+		assertEquals(title, "Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah");
+	}
+		
+	@Test
+	public void testNav1()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "Ski Utah Trip Planner - Ski Utah";
+		String menuOption = "Plan Your Trip";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	
+	@Test
+	public void testNav2()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "Utah Snow Report | Snow Totals at Utah Ski Resorts - Ski Utah";
+		String menuOption = "Resorts & Snow";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	
+	
+	@Test
+	public void testNav3()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "Read About the Latest Happenings on the Slopes - Ski Utah";
+		String menuOption = "Stories";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	
+	@Test
+	public void testNav4()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah";
+		String menuOption = "Deals";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	@Test
+	public void testNav5()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "2017-2018 Ski Season Passes | Utah Ski Passes | Ski Utah - Ski Utah";
+		String menuOption = "Passes";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	@Test
+	public void testNav6()
+	{
+		TestObjects.goToURL("http://www.skiutah.com");
+		String validationString = "Utah Ski Areas 101 | Utah Ski Resort Info | Ski Utah - Ski Utah";
+		String menuOption = "Explore";
+		assertTrue(TestObjects.verifyPage(validationString, menuOption));
+	}
+	
 }
