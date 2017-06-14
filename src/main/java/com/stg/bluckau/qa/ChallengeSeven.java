edc3318a7dba4835f4246b5959f4b390fd07d548
brian.luckau <brian.luckau@stgconsulting.com>
2017-06-14 14:50:18 -0600

    main work for Challenge 7

---
+++ src/main/java/com/stg/bluckau/qa/ChallengeSeven.java
@@ -0,0 +1,23 @@
+package com.stg.bluckau.qa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChallengeSeven
+{
+	public static void main( String[] args )
+	{
+		List<String> prefixes = new ArrayList<String>();
+
+		prefixes.add("http://skiutah.com");
+		prefixes.add("https://skiutah.com");
+		prefixes.add("http://www.skiutah.com");
+		prefixes.add("https://www.skiutah.com");
+
+		WebCrawler webCrawler = new WebCrawler("https://skiutah.com", prefixes, "c:\\tmp\\data.txt");
+		// webCrawler.recursivelyWalk("https://skiutah.com", 5, 5);
+		webCrawler.recursivelyWalk("https://skiutah.com");
+		webCrawler.printBrokenLinks();
+		Automation.quit();
+	}
+}
---
+++ src/main/java/com/stg/bluckau/qa/WordScraper.java
@@ -0,0 +1,78 @@
+package com.stg.bluckau.qa;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WordScraper
+{
+	private static WebDriver driver;
+	private HashMap<String, Integer> wordMap;
+
+	public WordScraper()
+	{
+		driver = Automation.getDriver();
+		wordMap = new HashMap<String, Integer>();
+	}
+
+	public void scrapePage()
+	{
+		List<WebElement> elementList = driver.findElements(By.xpath("//*"));
+		// System.out.println("Size of list is: " + elementList.size());
+		int prevNum;
+		String elemText = "";
+		for (WebElement webElement : elementList)
+		{
+			try
+			{
+				elemText = webElement.getText().replace("\n", " ").trim();
+			} catch (org.openqa.selenium.StaleElementReferenceException e)
+			{
+				System.err.println("Warning: Stale element");
+			}
+
+			String[] elemStrings = elemText.split(" ");
+			for (String elem : elemStrings)
+			{
+				prevNum = (wordMap.containsKey(elem)) ? wordMap.get(elem) : 0;
+				if (!"".equals(elem) && elem.matches("[a-zA-Z]+"))
+				{
+					// if (elem.startsWith("Valley"))
+					// System.out.println(" FOUND VALLEY");
+					wordMap.put(elem, ++prevNum);
+				}
+			}
+		}
+	}
+
+
+	public void writeFile(String fileName)
+	{
+		System.out.println("Writing file of dictionary words to: " + fileName);
+		try
+		{
+			OutputStream output = new FileOutputStream(fileName);
+			Properties props = new Properties();
+
+			for (String string : wordMap.keySet())
+			{
+				props.put(string, (wordMap.get(string)).toString());
+			}
+
+			props.store(output, null);
+
+			output.close();
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
