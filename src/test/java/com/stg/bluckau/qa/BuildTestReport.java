d5e8e3f1282e423558f313bf157c7ae63a56bed1
U-DESKTOP-8SPURV5\Brian Luckau <brian.luckau@stgconsulting.com>
2017-07-07 10:44:35 -0600

    Checking in changes from yesterday to help testing of jenkins at this juncture

---
+++ src/test/java/com/stg/bluckau/qa/BuildTestReport.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+// package com.stg.bluckau.qa;
+
+// import org.testng.ITestContext;
+
+/**
+ * @author Brian Luckau
+ *
+ */
+// public class BuildTestReport
+// {
+// ublic BuildTestReport(String level, ITestContext context)
+//
+//// RealTimeListener rl = new RealTimeListener();
+/// rl.`
+//
+//
